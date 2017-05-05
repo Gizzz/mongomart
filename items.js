7bef560fd43a88f8d3dd48a99c168d64eb140839
Gizzz <bulatkhusainov@yandex.ru>
2017-05-05 13:01:59 +0300

    fix indentation in items.js

--- items.js
+++ items.js
@@ -16,237 +16,237 @@
 
 
 var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+	assert = require('assert');
 
 
 function ItemDAO(database) {
-    "use strict";
+	"use strict";
 
-    this.db = database;
+	this.db = database;
 
-    this.getCategories = function(callback) {
-        this.db.collection("item")
-            .aggregate([
-                {
-                    "$group": { 
-                        "_id": { "category": "$category" },
-                        "count": { "$sum": 1 }
-                    }
-                },
-                {
-                    "$sort": { "_id": 1 }
-                },
-            ])
-            .toArray()
-            .then((groupsByCategory) => { 
-                const mappedGroups = groupsByCategory.map((group) => { 
-                    return { 
-                        _id: group._id.category, 
-                        num: group.count,
-                    }; 
-                });
+	this.getCategories = function (callback) {
+		this.db.collection("item")
+			.aggregate([
+				{
+					"$group": {
+						"_id": { "category": "$category" },
+						"count": { "$sum": 1 }
+					}
+				},
+				{
+					"$sort": { "_id": 1 }
+				},
+			])
+			.toArray()
+			.then((groupsByCategory) => {
+				const mappedGroups = groupsByCategory.map((group) => {
+					return {
+						_id: group._id.category,
+						num: group.count,
+					};
+				});
 
-                const overallCount = groupsByCategory.reduce((prev, next) => {
-                    return prev + next.count;
-                }, 0);
+				const overallCount = groupsByCategory.reduce((prev, next) => {
+					return prev + next.count;
+				}, 0);
 
-                const allItems_group = {
-                    _id: "All",
-                    num: overallCount,
-                };
+				const allItems_group = {
+					_id: "All",
+					num: overallCount,
+				};
 
-                const allGroups = [ allItems_group ].concat(mappedGroups);
-                callback(allGroups);
-             })
-             .catch((err) => { 
-                console.log(err);
-                throw err;
-             });
-    }
-
-
-    this.getItems = function(category, page, itemsPerPage, callback) {
-        let query = category === "All" ? {} : { category };
-
-        this.db.collection("item")
-            .find(query)
-            .sort({ "_id": 1 })
-            .skip(page * itemsPerPage)
-            .limit(itemsPerPage)
-            .toArray()
-            .then(pageItems => callback(pageItems))
-            .catch((err) => { 
-                console.log(err);
-                throw err;
-             });
-    }
+				const allGroups = [allItems_group].concat(mappedGroups);
+				callback(allGroups);
+			})
+			.catch((err) => {
+				console.log(err);
+				throw err;
+			});
+	}
 
 
-    this.getNumItems = function(category, callback) {
-        let query = category === "All" ? {} : { category };
+	this.getItems = function (category, page, itemsPerPage, callback) {
+		let query = category === "All" ? {} : { category };
 
-        this.db.collection("item")
-            .find(query)
-            .count()
-            .then(res => callback(res))
-            .catch((err) => { 
-                console.log(err);
-                throw err;
-             });
-    }
-
-
-    this.searchItems = function(query, page, itemsPerPage, callback) {
-        "use strict";
-
-        /*
-         * TODO-lab2A
-         *
-         * LAB #2A: Implement searchItems()
-         *
-         * Using the value of the query parameter passed to searchItems(),
-         * perform a text search against the "item" collection.
-         *
-         * Sort the results in ascending order based on the _id field.
-         *
-         * Select only the items that should be displayed for a particular
-         * page. For example, on the first page, only the first itemsPerPage
-         * matching the query should be displayed.
-         *
-         * Use limit() and skip() and the method parameters: page and
-         * itemsPerPage to select the appropriate matching products. Pass these
-         * items to the callback function.
-         *
-         * searchItems() depends on a text index. Before implementing
-         * this method, create a SINGLE text index on title, slogan, and
-         * description. You should simply do this in the mongo shell.
-         *
-         */
-
-        var item = this.createDummyItem();
-        var items = [];
-        for (var i=0; i<5; i++) {
-            items.push(item);
-        }
-
-        // TODO-lab2A Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the items for the selected page
-        // of search results to the callback.
-        callback(items);
-    }
+		this.db.collection("item")
+			.find(query)
+			.sort({ "_id": 1 })
+			.skip(page * itemsPerPage)
+			.limit(itemsPerPage)
+			.toArray()
+			.then(pageItems => callback(pageItems))
+			.catch((err) => {
+				console.log(err);
+				throw err;
+			});
+	}
 
 
-    this.getNumSearchItems = function(query, callback) {
-        "use strict";
+	this.getNumItems = function (category, callback) {
+		let query = category === "All" ? {} : { category };
 
-        var numItems = 0;
-
-        /*
-        * TODO-lab2B
-        *
-        * LAB #2B: Using the value of the query parameter passed to this
-        * method, count the number of items in the "item" collection matching
-        * a text search. Pass the count to the callback function.
-        *
-        * getNumSearchItems() depends on the same text index as searchItems().
-        * Before implementing this method, ensure that you've already created
-        * a SINGLE text index on title, slogan, and description. You should
-        * simply do this in the mongo shell.
-        */
-
-        callback(numItems);
-    }
+		this.db.collection("item")
+			.find(query)
+			.count()
+			.then(res => callback(res))
+			.catch((err) => {
+				console.log(err);
+				throw err;
+			});
+	}
 
 
-    this.getItem = function(itemId, callback) {
-        "use strict";
+	this.searchItems = function (query, page, itemsPerPage, callback) {
+		"use strict";
 
-        /*
-         * TODO-lab3
-         *
-         * LAB #3: Implement the getItem() method.
-         *
-         * Using the itemId parameter, query the "item" collection by
-         * _id and pass the matching item to the callback function.
-         *
-         */
+		/*
+		 * TODO-lab2A
+		 *
+		 * LAB #2A: Implement searchItems()
+		 *
+		 * Using the value of the query parameter passed to searchItems(),
+		 * perform a text search against the "item" collection.
+		 *
+		 * Sort the results in ascending order based on the _id field.
+		 *
+		 * Select only the items that should be displayed for a particular
+		 * page. For example, on the first page, only the first itemsPerPage
+		 * matching the query should be displayed.
+		 *
+		 * Use limit() and skip() and the method parameters: page and
+		 * itemsPerPage to select the appropriate matching products. Pass these
+		 * items to the callback function.
+		 *
+		 * searchItems() depends on a text index. Before implementing
+		 * this method, create a SINGLE text index on title, slogan, and
+		 * description. You should simply do this in the mongo shell.
+		 *
+		 */
 
-        var item = this.createDummyItem();
+		var item = this.createDummyItem();
+		var items = [];
+		for (var i = 0; i < 5; i++) {
+			items.push(item);
+		}
 
-        // TODO-lab3 Replace all code above (in this method).
+		// TODO-lab2A Replace all code above (in this method).
 
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the matching item
-        // to the callback.
-        callback(item);
-    }
-
-
-    this.getRelatedItems = function(callback) {
-        "use strict";
-
-        this.db.collection("item").find({})
-            .limit(4)
-            .toArray(function(err, relatedItems) {
-                assert.equal(null, err);
-                callback(relatedItems);
-            });
-    };
-
-
-    this.addReview = function(itemId, comment, name, stars, callback) {
-        "use strict";
-
-        /*
-         * TODO-lab4
-         *
-         * LAB #4: Implement addReview().
-         *
-         * Using the itemId parameter, update the appropriate document in the
-         * "item" collection with a new review. Reviews are stored as an
-         * array value for the key "reviews". Each review has the fields:
-         * "name", "comment", "stars", and "date".
-         *
-         */
-
-        var reviewDoc = {
-            name: name,
-            comment: comment,
-            stars: stars,
-            date: Date.now()
-        }
-
-        // TODO replace the following two lines with your code that will
-        // update the document with a new review.
-        var doc = this.createDummyItem();
-        doc.reviews = [reviewDoc];
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the updated doc to the
-        // callback.
-        callback(doc);
-    }
+		// TODO Include the following line in the appropriate
+		// place within your code to pass the items for the selected page
+		// of search results to the callback.
+		callback(items);
+	}
 
 
-    this.createDummyItem = function() {
-        "use strict";
+	this.getNumSearchItems = function (query, callback) {
+		"use strict";
 
-        var item = {
-            _id: 1,
-            title: "Gray Hooded Sweatshirt",
-            description: "The top hooded sweatshirt we offer",
-            slogan: "Made of 100% cotton",
-            stars: 0,
-            category: "Apparel",
-            img_url: "/img/products/hoodie.jpg",
-            price: 29.99,
-            reviews: []
-        };
+		var numItems = 0;
 
-        return item;
-    }
+		/*
+		* TODO-lab2B
+		*
+		* LAB #2B: Using the value of the query parameter passed to this
+		* method, count the number of items in the "item" collection matching
+		* a text search. Pass the count to the callback function.
+		*
+		* getNumSearchItems() depends on the same text index as searchItems().
+		* Before implementing this method, ensure that you've already created
+		* a SINGLE text index on title, slogan, and description. You should
+		* simply do this in the mongo shell.
+		*/
+
+		callback(numItems);
+	}
+
+
+	this.getItem = function (itemId, callback) {
+		"use strict";
+
+		/*
+		 * TODO-lab3
+		 *
+		 * LAB #3: Implement the getItem() method.
+		 *
+		 * Using the itemId parameter, query the "item" collection by
+		 * _id and pass the matching item to the callback function.
+		 *
+		 */
+
+		var item = this.createDummyItem();
+
+		// TODO-lab3 Replace all code above (in this method).
+
+		// TODO Include the following line in the appropriate
+		// place within your code to pass the matching item
+		// to the callback.
+		callback(item);
+	}
+
+
+	this.getRelatedItems = function (callback) {
+		"use strict";
+
+		this.db.collection("item").find({})
+			.limit(4)
+			.toArray(function (err, relatedItems) {
+				assert.equal(null, err);
+				callback(relatedItems);
+			});
+	};
+
+
+	this.addReview = function (itemId, comment, name, stars, callback) {
+		"use strict";
+
+		/*
+		 * TODO-lab4
+		 *
+		 * LAB #4: Implement addReview().
+		 *
+		 * Using the itemId parameter, update the appropriate document in the
+		 * "item" collection with a new review. Reviews are stored as an
+		 * array value for the key "reviews". Each review has the fields:
+		 * "name", "comment", "stars", and "date".
+		 *
+		 */
+
+		var reviewDoc = {
+			name: name,
+			comment: comment,
+			stars: stars,
+			date: Date.now()
+		}
+
+		// TODO replace the following two lines with your code that will
+		// update the document with a new review.
+		var doc = this.createDummyItem();
+		doc.reviews = [reviewDoc];
+
+		// TODO Include the following line in the appropriate
+		// place within your code to pass the updated doc to the
+		// callback.
+		callback(doc);
+	}
+
+
+	this.createDummyItem = function () {
+		"use strict";
+
+		var item = {
+			_id: 1,
+			title: "Gray Hooded Sweatshirt",
+			description: "The top hooded sweatshirt we offer",
+			slogan: "Made of 100% cotton",
+			stars: 0,
+			category: "Apparel",
+			img_url: "/img/products/hoodie.jpg",
+			price: 29.99,
+			reviews: []
+		};
+
+		return item;
+	}
 }
 
 
